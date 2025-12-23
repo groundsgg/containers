d482e52892ecc5523201aaf93adc55f15a0ff112
ItsKev <kevin.huber@hotmail.com>
2025-12-23 15:22:00 +0100

    feat: add velocity image

---
+++ velocity/scripts/start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set -eu
+
+exec java -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar /app/velocity.jar

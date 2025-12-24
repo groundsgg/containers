119d50311fc0b7a9ec33fedb7ef53b203e390b07
Lukas Jost <lukas.jost@outlook.com>
2025-12-24 21:40:02 +0100

    fix(velocity): align startup flags with docs

--- velocity/scripts/start.sh
+++ velocity/scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -eu
 
-exec java -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar /app/velocity.jar
+exec java -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:MaxInlineLevel=15 -jar /app/velocity.jar
